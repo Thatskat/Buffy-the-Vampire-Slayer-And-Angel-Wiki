507337c3059161608eaf257aff85f13593ab4e00
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-05-31 20:04:06 +1000

    changed production url

--- next.config.js
+++ next.config.js
@@ -22,7 +22,7 @@ module.exports = (phase) => {
   const env = {
     SERVER_NAME: (() => {
       if(isDev) return 'http://localhost:3000/';
-      if(isProd) return 'https://buffy-the-vampire-slayer-and-angel-wiki-nuikstoay-thatskat.vercel.app/'
+      if(isProd) return 'https://buffy-the-vampire-slayer-and-angel-wiki.vercel.app/'
     })(),
   
     NEWS_API_KEY: process.env.NEWS_API_KEY,
